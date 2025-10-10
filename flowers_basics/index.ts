bdad4eed403f4f7622bd0899706610f56cddeaa0
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 08:31:43 +0200

    Saved in TypeScript Playground at 2025-10-10T06:31:42.928Z

---
+++ flowers_basics/index.ts
@@ -0,0 +1,10 @@
+// <<< ADD CONSTANTS HERE (if you need them)
+
+function setup() {
+  // <<< ADD YOUR CODE HERE
+  createCanvas(500, 500)
+  background("white")
+
+  circle(100, 100, 70);
+  circle(150, 100, 70);
+}
