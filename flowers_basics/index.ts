e386502b2f3dda3c11a7221113910919ebeee924
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:34:32 +0200

    Saved in TypeScript Playground at 2025-10-10T07:34:31.953Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -2,18 +2,47 @@
 
 function setup() {
   // <<< ADD YOUR CODE HERE
-  createCanvas(500, 500)
-  background("white")
+  createCanvas(700, 600)
+  background("cyan")
+
+  // left arc
+  angleMode(DEGREES);
+  stroke("green")
+  strokeWeight(20)
+  noFill()
+  arc(60, 290, 300, 300, -50, 50)
 
   noStroke()
   strokeWeight(0)
-
+  //left flower
   fill("violet")
   circle(100, 110, 70);
   circle(155, 100, 70);
   circle(90, 165, 70);
-  circle(145, 190, 70);
-  circle(185, 145, 70);
+  circle(145, 145+ 47.5, 70);
+  circle(185, 150, 70);
   fill("yellow")
-  circle(135, 145, 70);
+  circle(135, 145, 65);
+
+  //right arc
+  angleMode(DEGREES);
+  stroke("green")
+  strokeWeight(20)
+  noFill()
+  arc(275, 300, 300, 300, -45, 45)
+
+  //right flower
+  stroke("darkred")
+  strokeWeight(2)
+  fill("red")
+  circle(340, 100, 80);
+  circle(295, 150, 80);
+  circle(340, 150+50, 80);
+  circle(385, 150, 80);
+  noStroke()
+  fill("orange")
+  circle(340, 150, 65);
+
+  fill("lightgreen")
+  rect(0, 400, 700, 300)
 }
