44f69581cfae7c6270fc6d125504372ce53ca42c
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 09:49:09 +0100

    Saved in TypeScript Playground at 2025-11-07T08:49:09.398Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -6,7 +6,7 @@ function setup() {
 
 // <<< Add the function `mouseMoved` with the required code here
 function mouseMoved() {
-
+    const gridSize=20
     background("black");
 
     stroke("white")
@@ -14,15 +14,25 @@ function mouseMoved() {
     noFill();
 
     let a=(width/2-mouseX)*2
-    let b= ((height/2- mouseY)*2)
+    let b= (height/2- mouseY)*2
 
     rect(mouseX, mouseY, (width/2- mouseX)*2, (height/2- mouseY)*2)
    
     strokeWeight(1);
-    text(`Area: ${b}`, 5, height - 10);
+    text(`Area: ${b*a}`, 5, height - 10);
 
-    stroke("lightgray");
     strokeWeight(1);
-    line(width*2 - a*2, height*2, width*2 + a*2, height*2);
-    line(width*2, height*2 - b*2, width*2, height*2 + b*2);
+    line(mouseX, mouseY, mouseX+ a, mouseY+b);
+    line(mouseX+a, mouseY, mouseX, mouseY+b);
+
+    stroke("red")
+    strokeWeight(2);
+    line(mouseX, mouseY, mouseX, mouseY+b,mouseX, mouseY)
+    stroke("lightgreen")
+    line(mouseX, mouseY, mouseX+a, mouseY, mouseX, mouseY)
+    stroke("yellow")
+    line(mouseX, mouseY+b, mouseX+a, mouseY+b, mouseX, mouseY)
+    stroke("blue")
+    line(mouseX+a, mouseY+b, mouseX+a, mouseY, mouseX, mouseY)
+
 }
