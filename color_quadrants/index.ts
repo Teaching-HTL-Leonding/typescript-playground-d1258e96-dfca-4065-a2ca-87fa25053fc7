2af105dd9d702c76844be7cb1f201baae8da3d3b
Rainer Stropek <rainer@timecockpit.com>
2025-11-21 08:42:28 +0100

    Saved in TypeScript Playground at 2025-11-21T07:42:28.154Z

---
+++ color_quadrants/index.ts
@@ -0,0 +1,54 @@
+const half: number = 100
+
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    fill("orange") // <<< Add your code here
+    rect(0, 0, width / 2, height / 2)
+
+    fill("green")
+    rect(width / 2, 0, width / 2, height / 2)
+
+    fill("blue")
+    rect(0, height - half, width / 2, half)
+
+    fill("red")
+    rect(width / 2, height - half, width / 2, half)
+
+    fill("black")
+    rect(width / 4, height / 2.6, width / 2, height / 4)
+}
+
+// <<< Add the function `moveClicked` with the required code here
+function mouseClicked() {
+    fill("black")
+    rect(width / 4, height / 2.6, width / 2, height / 4)
+
+    noStroke()
+    fill("white")
+    textSize(40)
+    textAlign(CENTER, CENTER)
+
+    noStroke()
+    fill("white")
+    textSize(40)
+    textAlign(CENTER, CENTER)
+
+    let s: string
+    if (mouseX < width / 2) {
+        if(mouseX <height)
+        s = 'Orange'
+    }
+    else if (mouseX < width / 2 * 2) {
+        s = 'Green'
+    }
+    else if (mouseX < height / 2) {
+        s = 'Blue'
+    }
+    else {
+        s = 'Red'
+    }
+    text(s, width / 2, height - half);
+}
