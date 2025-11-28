a8aa30804b4fb15bf5cb36d396024ab98c39ff6c
Rainer Stropek <rainer@timecockpit.com>
2025-11-28 10:34:41 +0100

    Saved in TypeScript Playground at 2025-11-28T09:34:41.055Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -20,35 +20,35 @@ function setup() {
 
   let hex1: string
   if (digit1 === 10) { hex1 = "A" }
-  else if (digit1 === 11){hex1="B"}
-  else if (digit1 === 12){hex1="C"}
-  else if (digit1 === 13){hex1="D"}
-  else if (digit1 === 14){hex1="E"}
-  else if (digit1 === 15){hex1="F"}
-  else {hex1=`${digit1}`}
+  else if (digit1 === 11) { hex1 = "B" }
+  else if (digit1 === 12) { hex1 = "C" }
+  else if (digit1 === 13) { hex1 = "D" }
+  else if (digit1 === 14) { hex1 = "E" }
+  else if (digit1 === 15) { hex1 = "F" }
+  else { hex1 = `${digit1}` }
 
-    let hex2: string
+  let hex2: string
   if (digit2 === 10) { hex2 = "A" }
-  else if (digit2 === 11){hex2="B"}
-  else if (digit2 === 12){hex2="C"}
-  else if (digit2 === 13){hex2="D"}
-  else if (digit2 === 14){hex2="E"}
-  else if (digit2 === 15){hex2="F"}
-  else {hex2=`${digit2}`}
+  else if (digit2 === 11) { hex2 = "B" }
+  else if (digit2 === 12) { hex2 = "C" }
+  else if (digit2 === 13) { hex2 = "D" }
+  else if (digit2 === 14) { hex2 = "E" }
+  else if (digit2 === 15) { hex2 = "F" }
+  else { hex2 = `${digit2}` }
 
-    let hex3: string
+  let hex3: string
   if (digit3 === 10) { hex3 = "A" }
-  else if (digit3 === 11){hex3="B"}
-  else if (digit3 === 12){hex3="C"}
-  else if (digit3 === 13){hex3="D"}
-  else if (digit3 === 14){hex3="E"}
-  else if (digit3 === 15){hex3="F"}
-  else {hex3=`${digit3}`}
+  else if (digit3 === 11) { hex3 = "B" }
+  else if (digit3 === 12) { hex3 = "C" }
+  else if (digit3 === 13) { hex3 = "D" }
+  else if (digit3 === 14) { hex3 = "E" }
+  else if (digit3 === 15) { hex3 = "F" }
+  else { hex3 = `${digit3}` }
 
   fill("yellow")
   textSize(50)
   text(hex3, width / 3.6, height / 2.1)
-  text(hex2, width / 1.87, height / 2.1)
-  text(hex1, width / 1.3, height / 2.1)
+  text(hex2, width / 1.9, height / 2.1)
+  text(hex1, width / 1.33, height / 2.1)
 
 }
