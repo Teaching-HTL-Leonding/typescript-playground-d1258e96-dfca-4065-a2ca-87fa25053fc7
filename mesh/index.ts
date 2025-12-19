14ef03ab7323237db0e396e8d0dbe004bd3bacf7
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 10:36:38 +0100

    Saved in TypeScript Playground at 2025-12-19T09:36:38.047Z

--- mesh/index.ts
+++ mesh/index.ts
@@ -5,12 +5,12 @@ const MARGIN = 50; // Margin between the edges and the rays.
 function setup() {
   createCanvas(SIZE, SIZE);
   background("black");
+  colorMode(HSB)
 
   strokeWeight(1);
-  stroke("lime");
 
   for (let j = MARGIN; j < SIZE; j += MARGIN) {
-
+    stroke(j-MARGIN, 100, 100)
     for (let i = MARGIN; i < SIZE; i += MARGIN) {
       line(SIZE-MARGIN , i, MARGIN, j);
     }
