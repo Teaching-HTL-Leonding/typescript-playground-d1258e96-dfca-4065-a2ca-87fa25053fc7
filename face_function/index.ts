6e9f2071b6045b5f832f43294e90694358957dba
Rainer Stropek <rainer@timecockpit.com>
2026-02-13 16:30:44 +0100

    Saved in TypeScript Playground at 2026-02-13T15:30:43.892Z

--- face_function/index.ts
+++ face_function/index.ts
@@ -1,3 +1,5 @@
+let nextSmiling = true; // We start with a happy face
+
 function setup() {
   createCanvas(500, 500);
   angleMode(DEGREES);
@@ -6,7 +8,12 @@ function setup() {
   // +------------------------------ Function name
   // |  +--------------------------- Function parameter
   // v  v
-  smile(true);
+  smile(nextSmiling); // Draw the first face
+}
+
+function mouseClicked() {
+  nextSmiling = !nextSmiling;
+  smile(nextSmiling);
 }
 
 // Function DEFINITION
@@ -15,5 +22,31 @@ function setup() {
 //       |     |      +------------- Parameter type
 //       v     v      v
 function smile(happy: boolean) {
-  // <<< Add your code here
+  push();
+  translate(random(0, width), random(0, height));
+  scale(0.2);
+
+  stroke("black");
+  strokeWeight(10);
+  if (happy) {
+    fill("yellow");
+  } else {
+    fill("lime");
+  }
+
+  circle(200, 200, 350);
+
+  fill("black");
+  circle(125, 125, 20);
+  circle(275, 125, 20);
+
+  noFill();
+
+  if (happy) {
+    arc(200, 250, 200, 150, 0, 180);
+  } else {
+    arc(200, 300, 200, 150, 180, 360);
+  }
+
+  pop();
 }
