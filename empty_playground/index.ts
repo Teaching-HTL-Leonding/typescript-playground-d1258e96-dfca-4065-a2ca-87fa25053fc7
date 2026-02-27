839edbc751f128a59a942b8d3cb54c020e50d5af
Rainer Stropek <rainer@timecockpit.com>
2026-02-27 10:12:47 +0100

    Saved in TypeScript Playground at 2026-02-27T09:12:47.494Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,24 +1,37 @@
-    function setup() {
-    createCanvas(400, 400)
-    background("hotpink")
-    }
-
-    let x: number=0
-    let direction: number=3
-
-    function draw() {
-    background("hotpink")
-    fill("darkblue")
-    circle(x, 200, 50)
-    x+= direction
-
-    // === gleich
-    // !== nicht gleich
-    // >,>=, <,<= Vergleich
-    if(x>=width){ direction=-3}
-    if (x===0) {direction=3}
-    }
-
-
-// Bedingungen
-// if Statement 
+let colorIndex = 0;
+let colors: string[] = ["red", "green", "blue"];
+ 
+function setup() {
+  createCanvas(500, 500);
+  background(255);
+}
+ 
+function mouseClicked() {
+  let x = random(50, width - 50);
+  let y = random(50, height - 50);
+ 
+  flower(x, y, colors[colorIndex]);
+ 
+  // nächste Farbe
+  colorIndex = (colorIndex + 1) % colors.length;
+}
+ 
+function flower(x: number, y: number, petalColor: string) {
+  push();
+  translate(x, y);
+ 
+  noStroke();
+  fill(petalColor);
+ 
+  // Blütenblätter
+  circle(0, -25, 50);
+  circle(25, 0, 50);
+  circle(0, 25, 50);
+  circle(-25, 0, 50);
+ 
+  // Mitte
+  fill("yellow");
+  circle(0, 0, 40);
+ 
+  pop();
+}
